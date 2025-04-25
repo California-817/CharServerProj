d28521628996b800a5b655f66308a82425a05948
xxxten <1876439531@qq.com>
2025-04-25 17:26:49 +0800

    cmake编译GateServer使用grpc

---
+++ GateServer/cmake/build/CMakeFiles/GateServer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/xxxten/ChatProj/CharServerProj/GateServer/bin/GateServer"
+  "/home/xxxten/ChatProj/CharServerProj/GateServer/bin/GateServer.pdb"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/CServer.cpp.o"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/CServer.cpp.o.d"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/GateServer.cpp.o"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/GateServer.cpp.o.d"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/HttpConnection.cpp.o"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/HttpConnection.cpp.o.d"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/LogicSystem.cpp.o"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/LogicSystem.cpp.o.d"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/VarifyGrpcClient.cpp.o"
+  "CMakeFiles/GateServer.dir/home/xxxten/ChatProj/CharServerProj/GateServer/src/VarifyGrpcClient.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GateServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
